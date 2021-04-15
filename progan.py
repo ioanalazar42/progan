072a16b481a1736355747549cd10a364a3a7a9a2
Ioana Lazar <ioanalazar459@gmail.com>
2021-04-15 14:11:35 +0000

    Add if statement to check if network size > 4 to condition the display of residual influence.

--- progan.py
+++ progan.py
@@ -191,15 +191,16 @@ for network_size in [4, 8, 16, 32, 64, 128]:
         time_elapsed = timer() - start_time
 
         # Log some statistics.
-        stats = f'{network_size}x{network_size} | {global_epoch_count:3} | {epoch:3} | '
+        stats = (f'{network_size}x{network_size} | {global_epoch_count:3} | {epoch:3} | '
             f'Loss(C): {average_critic_loss:.6f} | '
             f'Loss(G): {average_generator_loss:.6f} | '
             f'Avg C(x): {average_critic_real_performance:.6f} | '
             f'Avg C(G(x)): {average_critic_generated_performance:.6f} | '
             f'C(x) - C(G(x)): {discernability_score:.6f} | '
-            f'Time: {time_elapsed:.3f}s'
+            f'Time: {time_elapsed:.3f}s')
         # If network_size > 4, log residual influence for current network.
-        stats += f' | {int(network_size/2)}x{int(network_size/2)} residual influence: {generator_model.residual_influence:.3f}'
+        if network_size > 4:
+            stats += f' | {int(network_size/2)}x{int(network_size/2)} residual influence: {generator_model.residual_influence:.3f}'
         logger.info(stats)
 
         # Save models and tensorboard data.
