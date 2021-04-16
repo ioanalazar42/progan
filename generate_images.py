643f5826e26b25e32cea8fc885a289109ec812fb
Ioana Lazar <ioanalazar459@gmail.com>
2021-04-16 09:46:59 +0000

    Add script for generating images and improve generate_images.py

--- generate_images.py
+++ generate_images.py
@@ -4,34 +4,27 @@ import torch
 import torch.nn.functional as F
 import torchvision
 
-from networks.network import Generator128x128
+from networks.network5 import Generator128x128
 
-
-def _get_grid_size(num_images, max_len=20):
-    '''Returns the number of images that will be put in a grid of size 8 x num_images.
-       Grid size can be at most 8 x max_len.'''
-    if num_images == 0 or num_images == 1:
-        return 1
-    elif num_images > max_len:
-        return max_len *  max_len
-    else:
-        return num_images * num_images
 
 PARSER = argparse.ArgumentParser()
 PARSER.add_argument('--model_file_name',
                     default='final-128x128-generator.pth',
-                    help='The file name of a trained model.')
-PARSER.add_argument('--num_images',
-                    default='8', type=int,
-                    help='e.g. 15 -> grid of 15*15 images..')
+                    help='The file name of a trained model')
+PARSER.add_argument('--grid_size',
+                    default='64', type=int,
+                    help='Grid size -> [1, 64]')
+PARSER.add_argument('--lsv_size',
+                    default='512', type=int,
+                    help='Size of latent space vectors')
 args = PARSER.parse_args()
 
-GRID_SIZE = _get_grid_size(args.num_images)
+GRID_SIZE = min(64, max(1, args.grid_size))
 MODEL_PATH = f'trained_models/{args.model_file_name}'
 DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-SAVE_IMAGE_DIR = f'generated_with_preloaded_models/{args.model_file_name}'
+SAVE_IMAGE_DIR = f'generated_with_preloaded_models'
 
-if args.num_images > 1:
+if args.grid_size > 1:
     # Save grids to separate directory.
     SAVE_IMAGE_DIR += '/grids'
 else:
@@ -54,7 +47,7 @@ generator_model.eval()
 print(f'Loaded model {MODEL_PATH}')
 
 # Create a random batch of latent space vectors.
-fixed_latent_space_vectors = torch.randn([GRID_SIZE, 512], device=DEVICE)
+fixed_latent_space_vectors = torch.randn([GRID_SIZE, args.lsv_size], device=DEVICE)
 
 generated_images = generator_model(fixed_latent_space_vectors).detach()
 generated_images = F.interpolate(generated_images, size=(128, 128), mode='nearest')
